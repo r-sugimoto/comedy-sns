b5852b0e469acb52da3cbcc52c081fb1aba55b10
r.sugimoto <>
2021-03-14 13:36:52 +0900

    #32 会員登録機能のテスト作成

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
 	transform: {
 		".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
 		"^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+		"vee-validate/dist/rules": "babel-jest",
 	},
 	moduleFileExtensions: ["js", "vue"],
+	transformIgnorePatterns: [
+		"<rootDir>/node_modules/(?!(vuetify|vee-validate))",
+	],
 };
--- tests/Feature/Api/RegisterTest.php
+++ tests/Feature/Api/RegisterTest.php
@@ -58,30 +58,4 @@ class RegisterTest extends TestCase
                 'name' => 'test',
         ]);
     }
-
-    public function test_存在しないトークンの受け取り_本会員登録_失敗()
-    {
-        // トークン取得
-        $response = $this->get('/verification/413o24hi32j4324hio324i12j4i2i4u8-0bgqo3ptjdspokjgo');
-        // 仮会員登録に存在しないトークンであれば、ログインページに遷移する
-        $response->assertRedirect('login');
-        $response->assertStatus(302);
-    }
-
-    public function test_トークンの受け取り_本会員登録_正常()
-    {
-        // トークン取得
-        $response = $this->get('/verification/130c9275efd05b4d0bed20ded58d16cc46fcb9e237d5bb9c970fff02365187f6');
-        // 本会員登録チェック
-        // 仮会員登録情報を削除
-        $register_user = RegisterUser::where('email', $this->register_user->email)->first();
-        $this->assertNull($register_user);
-        // 本会員登録
-        $user = User::where('email', $this->register_user->email)->first();
-        $this->assertNotNull($user);
-        // 仮会員登録に存在するトークンであれば、ダッシュボードページに遷移する
-        $response->assertRedirect('dashboard');
-        $response->assertStatus(302);
-    }
-
 }
