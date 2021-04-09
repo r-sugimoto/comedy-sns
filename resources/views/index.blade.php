7ee461004e8f1e1adb0dac2475a4650bfaab11ff
r.sugimoto <>
2021-04-10 02:03:57 +0900

    favicon設置

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,14 @@
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+
 <title>Comenion</title>
-<!-- Styles -->
+<link rel="apple-touch-icon" href="{{ asset('images/apple-touch-icon.png') }}">
 <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
 </div>
-<!-- Scripts -->
 <script src="{{ mix('/js/app.js') }}" defer></script>
 </body>
 </html>
