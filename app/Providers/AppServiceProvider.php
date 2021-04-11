6875ee66c55d711e320215bf87697a60d31a3fea
r.sugimoto <>
2021-04-11 17:36:07 +0900

    常時SSL化修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +22,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        if (request()->isSecure()) {
-            \URL::forceScheme('https');
-        }
+        $url->forceScheme('https');
     }
 }
