27269abcbc91c4c6c51ffd1fc247d5db562d4ee2
r.sugimoto <>
2021-04-11 18:04:11 +0900

    サーチコンソールの設置、常時SSL化設定

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -17,7 +17,13 @@ class TrustProxies extends Middleware
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var int
+     * @var array
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,7 @@
 <link rel="apple-touch-icon" href="{{ asset('images/apple-touch-icon.png') }}">
 <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 @if(env('GA_ENABLE'))
+<meta name="google-site-verification" content="wfZmTz8u-N8atx0Fc7mkkslweqDQ-7yfFj8zLn78ahg" />
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139969751-4"></script>
 <script>
